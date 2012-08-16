dfe2d2b75ca25084328dbe7f3857e403178914e0
Takashi Ogura <t.ogura@gmail.com>
2012-08-16 20:36:43 +0900

    add ros, filter tests

---
+++ test/filter.js
@@ -0,0 +1,126 @@
+test('SearchFilter', function() {
+       var filter = new wxconsole.SearchFilter('aa');
+       equal(filter.name, 'aa');
+       equal(filter.enabled, true);
+       equal(filter.text, '');
+       equal(filter.include, true);
+       equal(filter.regex, false);
+       equal(filter.where['Message'], true);
+       equal(filter.where['Node'], true);
+       equal(filter.where['Location'], true);
+       equal(filter.where['Topics'], true);
+       
+     });
+
+test('SearchFilter.stringInclude', function() {
+       var filter = new wxconsole.SearchFilter('aa');
+       filter.text = 'hoge';
+       ok(filter.stringInclude('hogeaaa'));
+       ok(filter.stringInclude('bbbbhogeaaa'));
+       ok(!filter.stringInclude('bbbbhogaaa'));
+       filter.text = 'ho+ge';
+       ok(!filter.stringInclude('hogeaaa'));
+       ok(!filter.stringInclude('bbbbhogeaaa'));
+       ok(!filter.stringInclude('bbbbhogaaa'));
+       ok(!filter.stringInclude('bbbbhoooogaaa'));
+     });
+
+test('SearchFilter.regexInclude', function() {
+       var filter = new wxconsole.SearchFilter('aa');
+       filter.text = 'hoge';
+       ok(filter.regexInclude('hogeaaa'));
+       ok(filter.regexInclude('bbbbhogeaaa'));
+       ok(!filter.regexInclude('bbbbhogaaa'));
+       filter.text = 'ho+ge';
+       ok(filter.regexInclude('hogeaaa'));
+       ok(filter.regexInclude('bbbbhogeaaa'));
+       ok(filter.regexInclude('bbbbhogeaa'));
+       ok(filter.regexInclude('bbbbhoooooogeaa'));
+       ok(!filter.regexInclude('bbbbhgeaa'));
+     });
+
+test('SearchFilter.reject', function() {
+       var filter = new wxconsole.SearchFilter('aa');
+
+       filter.text = 'node1';
+       filter.enabled = false;
+       var msg = {
+	 name : '/node1',
+	 header : { stamp : {secs : 100,
+			     nsecs : 123},
+		    frame_id : 'frame1'},
+	 level : 4,
+	 file : 'main.c',
+	 function : 'Func()',
+	 line : 145,
+	 topics : ['/topic1', '/topic2', '/rosout'],
+	 msg : 'this is test message'
+       };
+       ok(!filter.reject(msg));
+       filter.enabled = true;
+       ok(!filter.reject(msg));
+       filter.text = 'aaaa';
+       ok(filter.reject(msg));
+       filter.text = 'node';
+       filter.where['Message'] = false;
+       filter.where['Location'] = false;
+       filter.where['Topics'] = false;
+       ok(!filter.reject(msg));
+       filter.text = 'nodeaaa';
+       ok(filter.reject(msg));
+       // include
+       
+     });
+
+test('SeverityFilter', function() {
+       var filter = new wxconsole.SeverityFilter();
+       equal(filter.name, 'SeverityFilter');
+       var msgDebug = {level: 1};
+       var msgInfo = {level: 2};
+       var msgWarn = {level: 4};
+       var msgError = {level: 8};
+       var msgFatal = {level: 16};
+       ok(!filter.reject(msgDebug));
+       ok(!filter.reject(msgInfo));
+       ok(!filter.reject(msgWarn));
+       ok(!filter.reject(msgError));
+       ok(!filter.reject(msgFatal));
+       filter.toggleSelectedLevel('Debug');
+
+       ok(filter.reject(msgDebug));
+       ok(!filter.reject(msgInfo));
+       ok(!filter.reject(msgWarn));
+       ok(!filter.reject(msgError));
+       ok(!filter.reject(msgFatal));
+       filter.toggleSelectedLevel('Info');
+       ok(filter.reject(msgDebug));
+       ok(filter.reject(msgInfo));
+       ok(!filter.reject(msgWarn));
+       ok(!filter.reject(msgError));
+       ok(!filter.reject(msgFatal));
+       filter.toggleSelectedLevel('Warn');
+       ok(filter.reject(msgDebug));
+       ok(filter.reject(msgInfo));
+       ok(filter.reject(msgWarn));
+       ok(!filter.reject(msgError));
+       ok(!filter.reject(msgFatal));
+       filter.toggleSelectedLevel('Error');
+       ok(filter.reject(msgDebug));
+       ok(filter.reject(msgInfo));
+       ok(filter.reject(msgWarn));
+       ok(filter.reject(msgError));
+       ok(!filter.reject(msgFatal));
+       filter.toggleSelectedLevel('Fatal');
+       ok(filter.reject(msgDebug));
+       ok(filter.reject(msgInfo));
+       ok(filter.reject(msgWarn));
+       ok(filter.reject(msgError));
+       ok(filter.reject(msgFatal));
+       filter.toggleSelectedLevel('Info');
+       ok(filter.reject(msgDebug));
+       ok(!filter.reject(msgInfo));
+       ok(filter.reject(msgWarn));
+       ok(filter.reject(msgError));
+       ok(filter.reject(msgFatal));
+});
+
---
+++ test/ros.js
@@ -0,0 +1,28 @@
+test('setRosbridgeVersion', function() {
+       wxconsole.setRosbridgeVersion('1.0');
+       equal(wxconsole.Adaptor, wxconsole.Rosbridge1Adaptor);
+       wxconsole.setRosbridgeVersion('2.0');
+       equal(wxconsole.Adaptor, wxconsole.Rosbridge2Adaptor);
+});
+
+test('Rosbridge1Adaptor', function() {
+       var controller = {
+       };
+       var adaptor = new wxconsole.Rosbridge1Adaptor('h1', 100, '/hoge', controller);
+       equal(adaptor.host, 'h1');
+       equal(adaptor.port, 100);
+       equal(adaptor.topic, '/hoge');
+       equal(adaptor.controller, controller);
+       equal(adaptor.ROSBRIDGE_VERSION, '1.0');
+});
+
+test('Rosbridge2Adaptor', function() {
+       var controller = {
+       };
+       var adaptor = new wxconsole.Rosbridge2Adaptor('h1', 100, '/hoge', controller);
+       equal(adaptor.host, 'h1');
+       equal(adaptor.port, 100);
+       equal(adaptor.topic, '/hoge');
+       equal(adaptor.controller, controller);
+       equal(adaptor.ROSBRIDGE_VERSION, '2.0');
+});
